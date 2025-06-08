d365643e22e1e25c0ab1c7801fb6df4f9bff4039
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-06-08 16:42:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -1,32 +1,33 @@
-import java.util.*;
+// User function Template for Java
 
 class Solution {
-    public int longestSubarray(int[] nums, int k) {
-        // Map to store the first occurrence of each prefix sum
-        Map<Integer, Integer> prefixMap = new HashMap<>();
-        int sum = 0;
-        int maxLen = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-
-            // Case 1: Whole array (from index 0) sums to k
-            if (sum == k) {
-                maxLen = i + 1;
-            }
-
-            // Case 2: There is a previous prefix sum that makes this subarray sum to k
-            if (prefixMap.containsKey(sum - k)) {
-                int prevIndex = prefixMap.get(sum - k);
-                maxLen = Math.max(maxLen, i - prevIndex);
-            }
-
-            // Only store the first occurrence of each prefix sum
-            if (!prefixMap.containsKey(sum)) {
-                prefixMap.put(sum, i);
-            }
+    public int longestSubarray(int[] arr, int k) {
+        // code here
+        
+        int sum = 0; int maxlength = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            
+            
+              if(sum == k){
+                maxlength = i + 1;
+            
         }
-
-        return maxLen;
+        if(map.containsKey(sum - k)){
+            int length = i - map.get(sum - k);
+            maxlength = Math.max(maxlength, length);
+        }
+        if(!map.containsKey(sum)){
+            map.put(sum, i);
+        }
+            
+        
+      
+        }
+        return maxlength;
+        
     }
 }
